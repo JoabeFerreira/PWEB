2f61f41f78a39ad161c9693383d61cf3dad64bf2
Joabe Ferreira <joa3.bys@hotmail.com>
2020-09-28 12:31:41 -0300

    Add files via upload

---
+++ Atividade 5/Media.js
@@ -0,0 +1,8 @@
+nome=prompt("Informe seu nome: ");
+nota1=parseFloat(prompt("Informe sua nota 1: "));
+nota2=parseFloat(prompt("Informe sua nota 2: "));
+nota3=parseFloat(prompt("Informe sua nota 3: "));
+
+Media=(nota1+nota2+nota3)/3;
+alert("Nome: "+ nome+"\nNota 1: "+nota1+"\nNota 2: "+nota2+"\nNota 3: "+ nota3);
+alert("Sua média é de: "+Media);
---
+++ Atividade 5/operacoes.js
@@ -0,0 +1,7 @@
+n1=parseFloat(prompt("Informe o Primeiro numero: "));
+n2=parseFloat(prompt("Informe o Segundo numero: "));
+alert("A soma dos dois é: "+ (n1+n2));
+alert("A subtração do primeiro pelo segundo é: "+ (n1-n2));
+alert("O produto dos dois é: "+ (n1*n2));
+alert("A divisão do primeiro pelo segundo é: "+ (n1/n2));
+alert("O resto da divisão do primeiro pelo segundo é: "+ (n1%n2));
