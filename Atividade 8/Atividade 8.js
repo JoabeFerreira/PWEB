dee724099c4f89b0eb66fc46ccabf5208cf65716
Joabe Ferreira <joa3.bys@hotmail.com>
2020-10-19 12:24:43 -0300

    Add files via upload

---
+++ Atividade 8/Atividade 8.js
@@ -0,0 +1,15 @@
+var aluno = {};
+
+aluno["RA"] = 0030481911018;
+aluno["Nome"] = "Joabe Ferreira";
+
+var aluno2 = {
+    RA: 0030481911040,
+    Nome: "Rafael Marinelli"
+};
+
+var aluno3 = {};
+aluno3.RA = 0030481911023;
+aluno3.Nome = "Julio Augusto"
+
+console.log(aluno, aluno2, aluno3)
