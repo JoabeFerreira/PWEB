cd34cea004a835eaba509cfee824a8f9bbbf8177
Joabe Ferreira <joa3.bys@hotmail.com>
2020-11-02 17:04:26 -0300

    Add files via upload

---
+++ Portfólio/Portifolio/portfolio.js
@@ -0,0 +1,29 @@
+cont=0;
+function msgProf(){
+    if(cont==0){
+        alert("Por favor professora, não ligue pra esse numero, é o meu numero de verdade");
+    }else if(cont==1){
+        alert("É serio Professora, Não!");       
+    }else if(cont==2){
+        alert("NÃO PROFESSORAAAA, EU LHE IMPLOROOOO");
+    }else{
+        alert("Ok vai em frente, pode ligar. Mas não garanto que vou responder ein!");
+    }
+    cont++;
+}
+
+function skillPy(){
+    alert("Dessa eu manjo, pode confiar");
+}
+function skillJs(){
+    alert("Essa eu tô aprendendo ainda");
+}
+function skillC(){
+    alert("É, nunca ganhei uma Interfatec usando ela mas do pro gasto");
+}
+function skillJava(){
+    alert("Essa eu só sei um pouquinho ainda, mas nada que um pouco de Dimas não resolva ;)");
+}
+function skillHtml(){
+    alert("Essa aí a senhora me diz :)");
+}
